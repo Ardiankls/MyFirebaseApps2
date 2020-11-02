b3366125dc26ba4343f2ff771c9654f9604f4743
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2020-11-03 06:17:39 +0700

    Initial commit

---
+++ app/src/main/java/com/uc/myfirebaseapss/adapter/SchedAdapter.java
@@ -0,0 +1,16 @@
+package com.uc.myfirebaseapss.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uc.myfirebaseapss.R;
+
+public class SchedAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sched_adapter);
+    }
+}
